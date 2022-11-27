a345d713a1607b8696c294c1afa181840c14bffc
Ghostovich <gusarov_konstant@mail.ru>
2022-11-27 12:22:07 +0300

    02

--- HW/1/3.py
+++ HW/1/3.py
@@ -5,3 +5,16 @@
 # - x=34; y=-30 -> 4
 # - x=2; y=4-> 1
 # - x=-34; y=-30 -> 3
+
+num1 = int(input('Введите X:'))
+num2 = int(input('Введите Y:'))
+if num1>0 and num2>0:
+    print("1")
+elif num1<0 and num2>0:
+    print('2')
+elif num1<0 and num2<0:
+    print('3')
+elif num1>0 and num2<0:
+    print('4')
+else:
+    print("Неверно!")
