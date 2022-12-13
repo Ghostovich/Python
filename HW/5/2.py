4172df9738d362cb3c5f8d49f6d51e5ae1b6e185
Ghostovich <gusarov_konstant@mail.ru>
2022-12-13 18:34:43 +0300

    hw5

--- HW/5/2.py
+++ HW/5/2.py
@@ -10,19 +10,6 @@
 # Входные и выходные данные хранятся в отдельных текстовых файлах.
 
 
-n = int(input('Введите число:'))
-sp = []
-
-i = 2
-while i*i<=n:
-    if n%i == 0:
-        sp.append(i)
-        n = n//i
-    else:
-        i+=1
-if n>1:
-    sp.append(n)
-print(sp)
 
     
     
