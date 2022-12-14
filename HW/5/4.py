bb1e7c257235f0f248516bed644d341ba491d5b8
Ghostovich <gusarov_konstant@mail.ru>
2022-12-15 01:55:43 +0300

    hw5.1

--- HW/5/4.py
+++ HW/5/4.py
@@ -1,2 +1,28 @@
 # Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
-# Входные и выходные данные хранятся в отдельных текстовых файлах.
+
+path = 'S/5/1.txt'
+data = open(path, 'r')
+a = data.read().split()
+a = list(map(int, a))
+print(a)
+data.close()
+
+
+
+path = 'S/5/1.txt'
+data = open(path, 'r')
+a = data.read().split()
+a = list(map(int, a))
+print(a)
+data.close()
+
+
+
+path = 'S/5/1.txt'
+data = open(path, 'r')
+a = data.read().split()
+a = list(map(int, a))
+print(a)
+data.close()
+
+
