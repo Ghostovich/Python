b9251d1f96c20279341e2f1e3d05f3d91e424421
Ghostovich <gusarov_konstant@mail.ru>
2022-12-19 22:50:37 +0300

    hw5.4

--- HW/5/4.py
+++ HW/5/4.py
@@ -1,52 +1,50 @@
 # Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
 
-count = 0
-sp = []
 path = 'HW/5/1.txt'
 data = open(path, 'r')
 for line in data:
-    a = str(line)
+    a = list(line)
 data.close()
 
-
-i = 0
-while i < len(a)+1:
-    if a[i] == a[i+1]:
+b = ''
+c = ''
+count = 1
+if not a:
+    c = ''
+for i in a:
+    if i != b:
+        if b:
+            c += str(count) + b
+        count = 1
+        b = i
+    else:
         count += 1
-        a1 = (f'{count}{a[i]}')
-    sp.append(a1)
-    i+=1
+else:
+    c += str(count) + b
 
 
+path = 'HW/5/2.txt'
+data = open(path, 'w')
+data.writelines(c)
+data.close()
 
+path = 'HW/5/1.txt'
+data = open(path, 'r')
+for line in data:
+    d = str(line)
+data.close()
 
+e = ''
+f = 1
+for x in d:
+    if str(x).isdigit():
+        f += x
+        print(x)
+    else:
+        e += x * int(f)
+        f = 1
 
-# path = 'HW/5/2.txt'
-# data = open(path, 'w')
-# data.writelines(sp)
-# data.close()
-
-# path = 'HW/5/1.txt'
-# data = open(path, 'r')
-# for line in data:
-#     b = str(line)
-# data.close()
-
-
-
-# i = 0
-# while i < len(A)-4:
-#     C = int(A[i])+int(B[i])
-#     C = str(f'{C} ')
-#     sp.append(C)
-#     i+=2
-# print(sp)
-
-
-
-
-
-# path = 'HW/5/3.txt'
-# data = open(path, 'w')
-# data.writelines(sp)
-# data.close()
+path = 'HW/5/3.txt'
+data = open(path, 'w')
+data.writelines(e)
+data.close()
