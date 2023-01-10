8a8fc611bd88792f09b68907ca7f0743d6ffbd9b
Ghostovich <gusarov_konstant@mail.ru>
2023-01-10 18:57:56 +0300

    hw9.5

--- HW/9/main.py
+++ HW/9/main.py
@@ -1,7 +1,5 @@
 import logging
-import models
 import sqlite3
-
 from telegram.ext import Updater, CommandHandler, ConversationHandler, MessageHandler, Filters
 
 logging.basicConfig(
@@ -9,6 +7,9 @@ logging.basicConfig(
 )
 
 logger = logging.getLogger(__name__)
+
+conn = sqlite3.connect('HW/9/contacts.db')
+cursor = conn.cursor()
 
 TOKEN = '5956752552:AAHT6H3L1H7pz_VBvYU7pK6DqxAW6BphSag'
 
@@ -21,69 +22,56 @@ def start(update, context):
         /delete - удалить данные сотрудника')
 
 
-
-# conn = sqlite3.connect('students.db')
-# cursor = conn.cursor()
-
-# # показать всех студентов
-# cursor.execute("select * from students")
-# results = cursor.fetchall()
-# print(results)
-
-# # поиск записи
-# surname = 'Иванов'
-# cursor.execute(f"select * from students where surname like '%{surname}%'")
-# results = cursor.fetchall()
-# print(results)
-
-# # добавить студента
-# name = 'Степан'
-# surname = 'Степанов'
-# phone = 45648
-# description = 'Инженер'
-# cursor.execute(
-#     f"insert into students (name, surname, phone, description) "
-#     f"values ('{name}', '{surname}', {phone}, '{description}')")
-# conn.commit()
-
-# # удалить студента
-# id = 5
-# cursor.execute(
-#     f"delete from students where id={id}"
-# )
-# conn.commit()
-
-# # обновить данные о студенте
-# id = 3
-# cursor.execute(
-#     f"update students set name='Юрий' where id={id}"
-# )
-# conn.commit()
-# conn.close()
+def show(update, context):
+    return 1
 
 
+def show1(update, context):
+    # # показать всех студентов
+    cursor.execute("select * from contacts")
+    results = cursor.fetchall()
+    update.message.reply_text(results)
+    print(results)
 
-# def calc(update, context):
-#     update.message.reply_text("Введите выражение: ")
+
+# def add(update, context):
 #     return 1
 
+# def add1(update, context):
+#     # добавить студента
+#     name = 'Степан'
+#     surname = 'Степанов'
+#     phone = 45648
+#     description = 'Инженер'
+#     cursor.execute(
+#         f"insert into students (name, surname, phone, description) "
+#         f"values ('{name}', '{surname}', {phone}, '{description}')")
+#     conn.commit()
 
-# def calculate(update, context):
-#         value = eval(update.message.text)
-#         update.message.reply_text(value)
-#         return ConversationHandler.END
 
-
-
-# def conv(update, context):
-#     update.message.reply_text("Введите массу в килограммах: ")
+# def delete(update, context):
 #     return 1
 
+# def delete1(update, context):
+    # # удалить студента
+    # id = 5
+    # cursor.execute(
+    #     f"delete from students where id={id}"
+    # )
+    # conn.commit()
 
-# def converter(update, context):
-#         value = int(update.message.text)
-#         update.message.reply_text(value*1000)
-#         return ConversationHandler.END
+
+# def change(update, context):
+#     return 1
+
+# def change1(update, context):
+    # # обновить данные о студенте
+    # id = 3
+    # cursor.execute(
+    #     f"update students set name='Юрий' where id={id}"
+    # )
+    # conn.commit()
+    # conn.close()
 
 
 def stop(update, context):
@@ -95,25 +83,40 @@ def main():
     updater = Updater(TOKEN)
     dp = updater.dispatcher
 
-    # calc_handler = ConversationHandler(
-    #     entry_points=[CommandHandler('calc', calc)],
+    show_handler = ConversationHandler(
+        entry_points=[CommandHandler('show', show)],
+        states={
+            1: [MessageHandler(Filters.text & ~Filters.command, show1)], },
+        fallbacks=[CommandHandler('stop', stop)]
+    )
+
+    # add_handler = ConversationHandler(
+    #     entry_points=[CommandHandler('add', add)],
     #     states={
-    #         1: [MessageHandler(Filters.text & ~Filters.command, calculate)], },
+    #         1: [MessageHandler(Filters.text & ~Filters.command, add1)], },
+    #     fallbacks=[CommandHandler('stop', stop)]
+
+    # change_handler = ConversationHandler(
+    #     entry_points=[CommandHandler('change', change)],
+    #     states={
+    #         1: [MessageHandler(Filters.text & ~Filters.command, change1)], },
     #     fallbacks=[CommandHandler('stop', stop)]
     # )
 
-    # conv_handler = ConversationHandler(
-    #     entry_points=[CommandHandler('conv', conv)],
+    # delete_handler = ConversationHandler(
+    #     entry_points=[CommandHandler('delete', delete)],
     #     states={
-    #         1: [MessageHandler(Filters.text & ~Filters.command, converter)], },
+    #         1: [MessageHandler(Filters.text & ~Filters.command, delete1)], },
     #     fallbacks=[CommandHandler('stop', stop)]
     # )
 
     start_handler = CommandHandler('start', start)
 
     dp.add_handler(start_handler)
-    # dp.add_handler(calc_handler)
-    # dp.add_handler(conv_handler)
+    dp.add_handler(show_handler)
+    # dp.add_handler(add_handler)
+    # dp.add_handler(change_handler)
+    # dp.add_handler(delete_handler)
     updater.start_polling()
     updater.idle()
 
